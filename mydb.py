907a3e5346f90ee678300a2f25c714a74d512b34
Ellen Fitzgerald <ellen.fitzgerald26@gmail.com>
2023-08-31 17:12:00 +0100

    Initial Commit

---
+++ mydb.py
@@ -0,0 +1,16 @@
+import mysql.connector
+
+dataBase = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    passwd = '1alfabetti_tallarines_beatles451'
+)
+
+# prepare a cursor object
+
+cursorObject = dataBase.cursor()
+
+# Create a database
+cursorObject.execute('CREATE DATABASE crmdb')
+
+print('All done')
