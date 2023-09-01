574ab4f00d84d01e8d7e4b136a25e2c47f387d13
Ellen Fitzgerald <ellen.fitzgerald26@gmail.com>
2023-09-01 16:11:41 +0100

    final project

--- crmsite/models.py
+++ crmsite/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Record(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    first_name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
+    email = models.CharField(max_length=100)
+    phone = models.CharField(max_length=20)
+    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=50)
+    country = models.CharField(max_length=50)
+    postcode = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return(f'{self.first_name} {self.surname}')
